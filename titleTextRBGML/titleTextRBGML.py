0487ba2d31d0c7c96b5b03045494a7ae68b05798
Rajat Monga <rajatmonga@Rajats-MacBook-Pro.local>
2018-07-13 20:43:13 -0500

    different approach

--- titleTextRBGML/titleTextRBGML.py
+++ titleTextRBGML/titleTextRBGML.py
@@ -2,13 +2,13 @@ import numpy as np
 
 class titleTextRBGML:
     def __init__(self):
-        self.text_color_map = np.array([[0,0,0],[0,1,1],[1,1,1,]])
-        self.red = None
-        self.green = None
-        self.blue = None
-
-        
-    def getColors(self):
         pass
+    
+    def getColorOfTitle(self, redValue, greenValue, blueValue):
+        L = (0.2126 * redValue) + (0.7152*greenValue) + (0.0722*blueValue)
+	if L > 0.179:
+	    return 'black'
+        else:
+            return 'white'
 
 
