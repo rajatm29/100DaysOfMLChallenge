077217e754cfd4924c0be9dd113f3c87913856df
Rajat Monga <rajatmonga29@gmail.com>
2018-07-28 12:52:52 -0500

    Update tensorFlowChatBot.py

--- tensorFlowChatBot/tensorFlowChatBot.py
+++ tensorFlowChatBot/tensorFlowChatBot.py
@@ -5,5 +5,6 @@ import seq2seq_model
 
 def train():
 
-    encoder_data, decoder_data = data_utils.prepare_custom_data()
+    encoder_data, decoder_data = data_utils.prepare_custom_data(gConfig['working_directory'])
+    train_set = read_data(encoder_data, decoder_data)
    
